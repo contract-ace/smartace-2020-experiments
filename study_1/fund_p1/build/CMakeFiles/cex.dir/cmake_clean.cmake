8c57d2c4dcc59c7a57e870a6a3fab6dab454f264
scottwe <scott.wesley@ns.sympatico.ca>
2020-10-14 13:42:11 -0400

    Case 1.

---
+++ study_1/fund_p1/build/CMakeFiles/cex.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
