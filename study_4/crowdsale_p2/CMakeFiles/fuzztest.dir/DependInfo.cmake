82cba8a77aae62da34d5f96c88e3df8fad7640bd
scottwe <scott.wesley@ns.sympatico.ca>
2021-01-25 17:43:38 -0500

    Crowdsale.

---
+++ study_4/crowdsale_p2/CMakeFiles/fuzztest.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aswesley/Research/tacas-2020-eval/study_4/crowdsale_p2/cmodel.cpp" "/home/aswesley/Research/tacas-2020-eval/study_4/crowdsale_p2/CMakeFiles/fuzztest.dir/cmodel.cpp.o"
+  "/home/aswesley/Research/tacas-2020-eval/study_4/crowdsale_p2/libverify/verify_libfuzzer.cpp" "/home/aswesley/Research/tacas-2020-eval/study_4/crowdsale_p2/CMakeFiles/fuzztest.dir/libverify/verify_libfuzzer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MC_USE_STDINT"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ study_4/crowdsale_p8/CMakeFiles/sea_inspect.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sea_inspect"
+  "merged.pp.ms.o.ll"
+  "sea_temps/merged.pp.ms.o.bc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/sea_inspect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
